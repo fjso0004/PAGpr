8e02c13273165446bd426c9f1cf575d9deb0f461
Javi <fjso0004@red.ujaen.es>
2024-09-13 19:18:24 +0200

    pr1 correcion errores cmake

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/admin/Desktop/pr1
+# Install script for directory: C:/Users/admin/Desktop/PAGpr
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/pr1")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/PAGpr")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/admin/Desktop/pr1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/admin/Desktop/PAGpr/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
